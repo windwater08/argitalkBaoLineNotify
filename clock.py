91dd38c0d254f3c07ea9028ecc87d1cfa265cb63
windwater08 <35369159+windwater08@users.noreply.github.com>
2022-02-12 23:27:04 +0800

    Add files via upload

--- clock.py
+++ clock.py
@@ -18,8 +18,8 @@ def scheduled_job():
 	dt2 = dt1.astimezone(timezone(timedelta(hours=8))) # 轉換時區 -> 東八區
 	#print('UTC \t%s\nUTC+8\t%s'%(dt1,dt2))
 	#print(dt2.strftime("%Y-%m-%d %H:%M:%S")) # 將時間轉換為 string
-    print('========== APScheduler CRON =========')
-    url = "https://argitalklinenotify.herokuapp.com/"
-    conn = urllib.request.urlopen(url)
+	print('========== APScheduler CRON =========')
+	url = "https://argitalklinenotify.herokuapp.com/"
+	conn = urllib.request.urlopen(url)
     
 sched.start()
